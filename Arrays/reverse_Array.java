5daa24043126a6c526d09e99ca7fd22640ae51b1
Namdev Yevtikar <namdevyevtikar148@gmail.com>
2026-01-17 18:46:08 +0530

    Initial Commit

---
+++ Arrays/reverse_Array.java
@@ -0,0 +1,18 @@
+public class reverse_Array {
+    public void reverseArray(int arr[]) {
+        // code here
+        int first =0;
+        int last=arr.length-1;
+        while(first<last){
+            int temp=arr[first];
+            arr[first]=arr[last];
+            arr[last]=temp;
+            
+            first++;
+            last--;
+            
+        }
+
+        
+    }
+}
